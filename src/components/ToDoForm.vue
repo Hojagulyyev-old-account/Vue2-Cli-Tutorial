23bef0ec75fd205321b9f3495a6eeea1fe684591
OwezdurdyPython <py.owezdurdy@gmail.com>
2021-10-01 19:12:36 +0500

    Global and single css files in VueApp

--- src/components/ToDoForm.vue
+++ src/components/ToDoForm.vue
@@ -1,16 +1,19 @@
 <template>
     <form @submit.prevent="onSubmit">
-        <label for="new-todo-input">
-            What needs to be done ?
-        </label>
+        <h2 class="label-wrapper">
+            <label for="new-todo-input" class="label__lg">
+                What needs to be done ?
+            </label>
+        </h2>
         <input
           type="text"
           id="new-todo-input"
           name="new-todo"
           autocomplete="off"
           v-model.lazy.trim="label"
+          class="input__lg"
         />
-        <button type="submit">Add</button>
+        <button type="submit" class="btn btn__primary btn__lg">Add</button>
     </form>
 </template>
 
@@ -18,7 +21,11 @@
 export default {
   methods: {
     onSubmit() {
+      if (this.label === ""){
+        return;
+      }
       this.$emit("todo-added", this.label);
+      this.label = "";
     }
   },
   data() {
